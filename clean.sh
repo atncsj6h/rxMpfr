7dcf812b97f4e9f0be099c9283a4fcbb7786fd42
Enrico Sorichetti <enricosorichetti@mac.com>
2023-03-29 04:06:43 +0200

    simplified the CMakeLists.txt

--- clean.sh
+++ clean.sh
@@ -1,11 +1,17 @@
 #! /usr/bin/env bash
 
-rm -f    CMakeCache.txt
-rm -f -r CMakeFiles
-rm -f    Makefile
-rm -f    cmake_install.cmake
-rm -f    *.o
-rm -f    *.a
-rm -f    *.so
-rm -f    *.dylib
-rm -f    vars_*
+find . -type f -name .DS_Store              -exec rm {} \;
+find . -type f -name a.out                  -exec rm {} \;
+
+find . -type f -name CMakeCache.txt         -exec rm {} \;
+find . -type d -name CMakeFiles             -exec rm -rf {} \;
+find . -type f -name Makefile               -exec rm {} \;
+
+find . -type f -name cmake_install.cmake    -exec rm {} \;
+
+find . -type f -name "*.o"                  -exec rm {} \;
+find . -type f -name "*.a"                  -exec rm {} \;
+find . -type f -name "*.so"                 -exec rm {} \;
+find . -type f -name "*.dylib"              -exec rm {} \;
+
+find . -type f -name "vars_*.txt"           -exec rm {} \;
